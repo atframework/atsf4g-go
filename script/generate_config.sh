c144960434c34d8b45b74c8e6b308a2249ab21df
yousongyang <yousongyang0105@gmail.com>
2025-10-28 11:34:45 +0800

    构建系统Linux支持

--- script/generate_config.sh
+++ script/generate_config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd "$(dirname $0)"
 
+chmod +x ./atdtool/bin/atdtool
 ./atdtool/bin/atdtool template ./deploy/charts -o ./ --values ./deploy/values/default,./deploy/values/dev --set global.world_id=1
